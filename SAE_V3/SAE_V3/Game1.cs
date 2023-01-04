9b5116a90ab485958564756d26a960572e479cbd
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2023-01-04 10:36:56 +0100

    ajout classe

--- SAE_V3/SAE_V3/Game1.cs
+++ SAE_V3/SAE_V3/Game1.cs
@@ -1,31 +1,79 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.Animations;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
 
-namespace SAE_V3
+namespace SAE
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private readonly ScreenManager _screenManager;
+        public SpriteBatch _spriteBatch { get; set; }
+        
+        
+
+        StartScreen startscreen;
+        GameScreen gamescreen;
+        EndScreen endscreen;
+
+        /*
+        //Joueur
+        private Vector2 _positionPlayer;
+        private AnimatedSprite _player;
+        Player playerDeBase = new Player("Player1", 100, 10, 0);
+
+       
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-        }
 
+            _screenManager = new ScreenManager();
+            Components.Add(_screenManager);
+        }
+       
         protected override void Initialize()
         {
+            
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.IsFullScreen = true;
+            _graphics.ApplyChanges();
 
+
+            //Joueur
+            _positionPlayer = new Vector2(_graphics.PreferredBackBufferWidth / 2 , _graphics.PreferredBackBufferHeight / 2);
+            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+
+            startscreen = new StartScreen(this); 
+            gamescreen = new GameScreen(this); 
+            endscreen = new EndScreen(this); 
+
+            _tiledMap = Content.Load<TiledMap>("map");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+
+            //Joueur
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("playerSide.sf", new JsonContentLoader());
+            _player = new AnimatedSprite(spriteSheet);
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -35,17 +83,65 @@ namespace SAE_V3
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            _tiledMapRenderer.Update(gameTime);
 
-            base.Update(gameTime);
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
+            float walkSpeed = deltaSeconds * playerDeBase.Vitesse; 
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+
+            //player bouge
+            string playerSide = "idle";
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                _positionPlayer.Y += 1;
+                playerSide = "walkNorth";
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                _positionPlayer.Y -= 1;
+                playerSide = "walkSouth";
+            }
+            else if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                _positionPlayer.X -= 1;
+                playerSide = "walkEast";
+
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                _positionPlayer.X += 1;
+                playerSide = "walkWest";
+            }
+           
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                _screenManager.LoadScreen(startscreen, new FadeTransition(GraphicsDevice,
+                Color.White));
+            }
+            else if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                _screenManager.LoadScreen(endscreen, new FadeTransition(GraphicsDevice,
+                Color.Black));
+            }
+            else
+            {
+
+            }
+
+             // TODO: Add your update logic here
+             _player.Play(playerSide);
+             _player.Update(deltaSeconds);
+             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
 
             // TODO: Add your drawing code here
-
             base.Draw(gameTime);
         }
     }
